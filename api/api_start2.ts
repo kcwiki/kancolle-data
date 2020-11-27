0b9e10ea40cd1a503baf740fbaa52f2f19e5abd7
gk <gkpyon@gmail.com>
2020-11-27 12:03:55 +0900

    0.201126.0: update

--- api/api_start2.ts
+++ api/api_start2.ts
@@ -192,6 +192,7 @@ export interface APIMstShipgraph {
 }
 
 export interface APIMstShipupgrade {
+  api_arms_mat_count: number
   api_aviation_mat_count: number
   api_catapult_count: number
   api_current_ship_id: number
@@ -242,12 +243,109 @@ export interface APIMstSlotitemEquiptype {
 }
 
 export interface APIMstStype {
-  api_equip_type: { [key: string]: number }
+  api_equip_type: APIEquipType
   api_id: number
   api_kcnt: number
   api_name: string
   api_scnt: number
   api_sortno: number
+}
+
+export interface APIEquipType {
+  '1': number
+  '10': number
+  '11': number
+  '12': number
+  '13': number
+  '14': number
+  '15': number
+  '16': number
+  '17': number
+  '18': number
+  '19': number
+  '2': number
+  '20': number
+  '21': number
+  '22': number
+  '23': number
+  '24': number
+  '25': number
+  '26': number
+  '27': number
+  '28': number
+  '29': number
+  '3': number
+  '30': number
+  '31': number
+  '32': number
+  '33': number
+  '34': number
+  '35': number
+  '36': number
+  '37': number
+  '38': number
+  '39': number
+  '4': number
+  '40': number
+  '41': number
+  '42': number
+  '43': number
+  '44': number
+  '45': number
+  '46': number
+  '47': number
+  '48': number
+  '49': number
+  '5': number
+  '50': number
+  '51': number
+  '52': number
+  '53': number
+  '54': number
+  '55': number
+  '56': number
+  '57': number
+  '58': number
+  '59': number
+  '6': number
+  '60': number
+  '61': number
+  '62': number
+  '63': number
+  '64': number
+  '65': number
+  '66': number
+  '67': number
+  '68': number
+  '69': number
+  '7': number
+  '70': number
+  '71': number
+  '72': number
+  '73': number
+  '74': number
+  '75': number
+  '76': number
+  '77': number
+  '78': number
+  '79': number
+  '8': number
+  '80': number
+  '81': number
+  '82': number
+  '83': number
+  '84': number
+  '85': number
+  '86': number
+  '87': number
+  '88': number
+  '89': number
+  '9': number
+  '90': number
+  '91': number
+  '92': number
+  '93': number
+  '94': number
 }
 
 export interface APIMstUseitem {
