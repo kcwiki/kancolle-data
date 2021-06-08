49bb2330cdffd1cdb1eb0a3d5f2ef4943bfccc5c
gk <gkpyon@gmail.com>
2021-06-08 10:36:16 +0900

    0.210602.1: update

--- asset/index.js
+++ asset/index.js
@@ -8,17 +8,19 @@ const key = s => s.split('').reduce((a, e) => a + e.charCodeAt(0), 0)
 
 const create = (id, type) => (((17 * (id + 7) * cgKeys[(key(type) + id * type.length) % 100]) % 8973) + 1000).toString()
 
-const ship = type => (id, damaged, server = '203.104.209.71') => {
-  const path = [
-    padStart(id, 4, '0'),
-    damaged && 'd',
-    create(id, `ship_${type}`),
-    (((type === 'full' || type === 'full_dmg') && api_mst_shipgraph.find(e => e.api_id === id)) || {}).api_filename,
-  ]
-    .filter(Boolean)
-    .join('_')
-  return `http://${server}/kcs2/resources/ship/${type}/${path}.png`
-}
+const ship =
+  type =>
+  (id, damaged, server = '203.104.209.71') => {
+    const path = [
+      padStart(id, 4, '0'),
+      damaged && 'd',
+      create(id, `ship_${type}`),
+      (((type === 'full' || type === 'full_dmg') && api_mst_shipgraph.find(e => e.api_id === id)) || {}).api_filename,
+    ]
+      .filter(Boolean)
+      .join('_')
+    return `http://${server}/kcs2/resources/ship/${type}/${path}.png`
+  }
 
 const shipBanner = ship('banner')
 const shipCard = ship('card')
@@ -34,8 +36,10 @@ const shipFull0Damaged = id => ship('character_full')(id, true)
 
 const shipVoice = (id, lineId) => (id <= 1500 && lineId <= 53 ? 100000 + ((17 * (id + 7) * voiceKeys[lineId - 1]) % 99173) : lineId)
 
-const equipment = type => (id, server = '203.104.209.71') =>
-  `http://${server}/kcs2/resources/slot/${type}/${padStart(id, 3, '0')}_${create(id, `slot_${type}`)}.png`
+const equipment =
+  type =>
+  (id, server = '203.104.209.71') =>
+    `http://${server}/kcs2/resources/slot/${type}/${padStart(id, 3, '0')}_${create(id, `slot_${type}`)}.png`
 
 const equipmentCard = equipment('card')
 const equipmentFull = equipment('item_on')
