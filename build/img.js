97c89aadd98a489211ebb87f5788424bb9c4a37c
gk <gkpyon@gmail.com>
2021-08-26 06:03:34 +0900

    feat: support generic urls in img script

--- build/img.js
+++ build/img.js
@@ -17,6 +17,12 @@
  *
  *     $ node build/img.js '' /tmp/kc/img
  *     $ node build/img.js common_event /tmp/kc/img
+ *
+ * Using a URL
+ *
+ *     $ node build/img.js kcs2/resources/map/051/01_image28
+ *     $ node build/img.js kcs2/resources/map/051/01_image28 /tmp/kc/img
+ *
  */
 
 const { each, map } = require('bluebird')
@@ -29,15 +35,18 @@ const dir = process.argv[3] || 'img'
 
 const cachePng = async (folder, assets) =>
   !existsSync(`${dir}/${assets}.png`) &&
-  outputFileSync(`${dir}/${assets}.png`, await (await fetch(`http://203.104.209.71/kcs2/img/${folder}/${assets}.png`)).buffer())
+  outputFileSync(
+    `${dir}/${assets}.png`,
+    await (await fetch(`http://203.104.209.71/${folder ? `kcs2/img/${folder}/${assets}.png` : `${assets}.png`}`)).buffer(),
+  )
 
 map(
   process.argv[2] ? process.argv[2].split(',') : require('../asset/image.json'),
   async assets => {
-    const folder = assets.startsWith('battle_result') ? 'battle_result' : assets.split('_')[0]
+    const folder = assets.startsWith('kcs2') ? null : assets.startsWith('battle_result') ? 'battle_result' : assets.split('_')[0]
     console.log(`${folder} ${assets}`)
     await cachePng(folder, assets)
-    const { frames } = await (await fetch(`http://203.104.209.71/kcs2/img/${folder}/${assets}.json`)).json()
+    const { frames } = await (await fetch(`http://203.104.209.71/${folder ? `kcs2/img/${folder}/${assets}.json` : `${assets}.json`}`)).json()
     each(toPairs(frames), ([frame, { frame: f }]) =>
       gm(`${dir}/${assets}.png`)
         .crop(f.w, f.h, f.x, f.y)
