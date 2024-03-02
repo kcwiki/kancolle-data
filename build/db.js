ac5ce106152e48f02479422effdc90f52627801d
gk <gkpyon@gmail.com>
2024-03-02 21:41:40 +0900

    fix db query

--- build/db.js
+++ build/db.js
@@ -16,10 +16,17 @@ const queryTsun = async (...qs) => {
   const tsun = new Client()
   await tsun.connect()
   for (const q of qs) {
-    const { rows } = await tsun.query(q.query)
-    result.push(rows)
-    if (q.file) {
-      outputJsonSync(`${__dirname}/../db/${q.file}.json`, q.reduce ? q.reduce(rows) : rows)
+    try {
+      console.log(`queryTsun: ${q.file}`)
+      const { rows } = await tsun.query(q.query)
+      const reducedRows = q.reduce ? q.reduce(rows) : rows
+      result.push(reducedRows)
+      if (q.file) {
+        outputJsonSync(`${__dirname}/../db/${q.file}.json`, reducedRows)
+      }
+    } catch (e) {
+      console.error(`queryTsun: ${q.query}`)
+      console.error(e)
     }
   }
   await tsun.end()
@@ -29,28 +36,28 @@ const queryTsun = async (...qs) => {
 const queryPoi = q =>
   new Promise(async resolve => {
     const data = {}
+    console.log(`queryPoi: ${q.file}`)
     Readable.from(gunzipSync(await (await fetch(`https://api.poi.moe/dump/${q.dump}.gz`)).buffer()))
       .pipe(new StreamBSON({ archive: true }))
       .on('data', e => q.map(data, e))
       .on('finish', () => {
-        outputJsonSync(`${__dirname}/../db/${q.file}.json`, q.reduce(data))
-        resolve()
+        const reducedData = q.reduce ? q.reduce(data) : data
+        if (q.file) {
+          outputJsonSync(`${__dirname}/../db/${q.file}.json`, reducedData)
+        }
+        resolve(reducedData)
       })
   })
 
-const eventIds = [57] // _.range(50, 60 + 1)
+const eventIds = [58] // _.range(50, 60 + 1)
 
-const mapQuery = () =>
-  eventIds
-    .map(eventId =>
-      _.range(1, 10 + 1)
-        .map(n => `map='${eventId}-${n}'`)
-        .join(' or '),
-    )
+const mapQuery = eventId =>
+  _.range(1, 10 + 1)
+    .map(n => `map='${eventId}-${n}'`)
     .join(' or ')
 
-const genNodeTypes = async () => {
-  const nodeTypes = require('../db/node_types_raw.json')
+const genNodeTypes = async eventId => {
+  const nodeTypes = require(`../db/node_types_raw-${eventId}.json`)
   const types = {}
   for (const { map, edge, eventid, eventkind, nodecolor } of nodeTypes) {
     const mapId = map.replace('-', '')
@@ -66,7 +73,7 @@ const genNodeTypes = async () => {
       types[mapId][label] = { type, id: eventid, kind: eventkind, color: nodecolor }
     }
   }
-  outputJsonSync(`${__dirname}/../db/node_types.json`, sortKeys(types, { deep: true }))
+  outputJsonSync(`${__dirname}/../db/node_types-${eventId}.json`, sortKeys(types, { deep: true }))
 }
 
 const main = async () => {
@@ -81,29 +88,6 @@ const main = async () => {
     reduce: e => _.mapValues(e, () => true),
   })
   await queryTsun(
-    // Poi: N/A
-    // {
-    //   query: `select * from friendlyfleet where ${mapQuery()}`,
-    //   file: 'ff',
-    // },
-    // Poi: N/A
-    // `select count(*)::int, map, edgeid[array_length(edgeid, 1)] as edge, (nodeinfo->>'nodeType')::int as nodeColor, (nodeinfo->>'eventKind')::int as eventKind, (nodeinfo->>'eventId')::int as eventId from normalworld where ${mapQuery()} group by map, edgeid[array_length(edgeid, 1)], nodeinfo->>'nodeType', nodeinfo->>'eventKind', nodeinfo->>'eventId'`
-    {
-      query: `select count(*)::int, map, edgeid[array_length(edgeid, 1)] as edge, (nodeinfo->>'nodeColor')::int as nodeColor, (nodeinfo->>'eventKind')::int as eventKind, (nodeinfo->>'eventId')::int as eventId from eventworld where ${mapQuery()} group by map, edgeid[array_length(edgeid, 1)], nodeinfo->>'nodeColor', nodeinfo->>'eventKind', nodeinfo->>'eventId'`,
-      file: 'node_types_raw',
-      reduce: data => data.filter(e => e.count > 1).map(e => _.omit(e, ['count'])),
-    },
-    // Poi: N/A
-    {
-      query: `select map, edgeid[array_length(edgeid, 1)] as edge, (nodeinfo->>'flavorMessage') as message, count(*)::int from eventworld where (${mapQuery()}) and (nodeinfo->>'flavorMessage') is not null and (nodeinfo->>'eventId')::int=6 group by map, edgeid[array_length(edgeid, 1)], nodeinfo->>'flavorMessage'`,
-      file: 'flavor',
-      reduce: data => data.filter(e => e.message && e.count > 1 && !e.message.includes('?????')).map(e => _.omit(e, ['count'])),
-    },
-    // Poi: missing selectreward
-    {
-      query: `select map, difficulty, rewards, selectreward from eventreward where ${mapQuery()} group by map, difficulty`,
-      file: 'reward',
-    },
     // Poi: N/A
     {
       query: 'select * from equips',
@@ -127,7 +111,38 @@ const main = async () => {
           .value(),
     },
   )
-  await genNodeTypes()
+  for (const eventId of eventIds) {
+    await queryTsun(
+      {
+        query: `select map, edgeid[array_length(edgeid, 1)] as edge, (nodeinfo->>'flavorMessage') as message, count(*)::int from eventworld where (${mapQuery(
+          eventId,
+        )}) and (nodeinfo->>'flavorMessage') is not null and (nodeinfo->>'eventId')::int=6 group by map, edgeid[array_length(edgeid, 1)], nodeinfo->>'flavorMessage'`,
+        file: `flavor-${eventId}`,
+        reduce: data => data.filter(e => e.message && e.count > 1 && !e.message.includes('?????')).map(e => _.omit(e, ['count'])),
+      },
+      // Poi: missing selectreward
+      {
+        query: `select map, difficulty, rewards, selectreward from eventreward where ${mapQuery(eventId)} group by map, difficulty`,
+        file: `reward-${eventId}`,
+      },
+      // Poi: N/A
+      // `select count(*)::int, map, edgeid[array_length(edgeid, 1)] as edge, (nodeinfo->>'nodeType')::int as nodeColor, (nodeinfo->>'eventKind')::int as eventKind, (nodeinfo->>'eventId')::int as eventId from normalworld where ${mapQuery(eventId)} group by map, edgeid[array_length(edgeid, 1)], nodeinfo->>'nodeType', nodeinfo->>'eventKind', nodeinfo->>'eventId'`
+      {
+        query: `select count(*)::int, map, edgeid[array_length(edgeid, 1)] as edge, (nodeinfo->>'nodeColor')::int as nodeColor, (nodeinfo->>'eventKind')::int as eventKind, (nodeinfo->>'eventId')::int as eventId from eventworld where ${mapQuery(
+          eventId,
+        )} group by map, edgeid[array_length(edgeid, 1)], nodeinfo->>'nodeColor', nodeinfo->>'eventKind', nodeinfo->>'eventId'`,
+        file: `node_types_raw-${eventId}`,
+        reduce: data => data.filter(e => e.count > 1).map(e => _.omit(e, ['count'])),
+      },
+      // Poi: N/A
+      // {
+      //   query: `select * from friendlyfleet where ${mapQuery(eventId)}`,
+      //   file: `ff-${eventId}`,
+      // },
+      // Poi: N/A
+    )
+    await genNodeTypes(eventId)
+  }
 }
 
 main()
