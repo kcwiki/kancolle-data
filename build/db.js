0df4bdb2ad5d8b3aa1d6a4a5daa64579a9fc53bf
gakada <gkpyon@gmail.com>
2025-05-18 06:35:49 +0900

    Update db.js

--- build/db.js
+++ build/db.js
@@ -49,7 +49,7 @@ const queryPoi = q =>
       })
   })
 
-const eventIds = +process.env.DB_EVENT_ID ? [+process.env.DB_EVENT_ID] : [60]
+const eventIds = +process.env.DB_EVENT_ID ? [+process.env.DB_EVENT_ID] : []
 
 const mapQuery = (eventId, mapId) => `(${(mapId ? [mapId] : _.range(1, 10 + 1)).map(mapId => `map='${eventId}-${mapId}'`).join(' or ')})`
 
